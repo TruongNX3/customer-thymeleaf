03e7987bd583040ec70441c15dec29cc79882cd0
TruongDepZai <nguyenxuantruong96.hust@gmail.com>
2018-11-05 11:55:52 +0700

    Ứng dụng quản lý khách hàng thêm tính năng thêm khách hàng mới

--- src/main/java/com/codegym/controllers/CustomerController.java
+++ src/main/java/com/codegym/controllers/CustomerController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.codegym.service.CustomerService;
 import com.codegym.service.CustomerServiceImpl;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
     private CustomerService customerService = new CustomerServiceImpl();
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public ModelAndView index(Model model) {
         model.addAttribute("customers", customerService.findAll());
         ModelAndView modelAndView = new ModelAndView("index");
@@ -25,5 +26,13 @@ public class CustomerController {
         ModelAndView modelAndView = new ModelAndView("create");
         model.addAttribute("customer", new Customer());
         return modelAndView;
+    }
+
+    @RequestMapping("/customer/save")
+    public String save(Model model, Customer customer) {
+        customer.setId((int)(Math.random()*10000));
+        customerService.save(customer);
+        model.addAttribute("success","Saved customer successfully!" );
+        return "redirect:/index";
     }
 }
